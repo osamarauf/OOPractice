5331d886176912bd474d27b6c0f5602a71a1642d
OsamaRauf <osama.rauf-intern@telenorgroup.com>
2024-04-29 23:52:09 +0500

    Practice Linear Search, String and Array Questions

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,8 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+E
+src/GenericClass.java,3\8\3863045b7b54fcb04feaf942fccb40f3f9cf8e9c
+:
+
+.gitignore,a\5\a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/Main.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GenericClass gc = new GenericClass();
+
+        gc.setName("Osama");
+        gc.setNum(3);
+
+        System.out.println(gc.getName() + gc.getNum());
+
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(0);
+        num.add(5);
+        num.add(0);
+        num.add(8);
+
+        for (int i = 0; i < num.size(); i++) {
+            if (num.get(i) %2==0){
+                num.remove(i);
+            }
+        }
+
+        //Move Zero's to end of the array
+        int count = 0;
+        for (int i = 0; i < num.size(); i++) {
+            if (num.get(i)!=0){
+                num.set(count++, num.get(i));
+            }
+        }
+
+        while (count < num.size()){
+            num.set(count++, 0);
+        }
+
+        System.out.println(num);
+        int[] arr = {1, 2, 3, 4};
+        int k =2;
+        int ans = linearSearch(arr, k);
+        System.out.println(ans);
+
+
+        // Leetcode 451 Sort Characters By Frequency
+        String s = "tree";
+        Map<Character, Integer> freqMap = new HashMap<>();
+        int maxFreq = 0;
+        char maxChar = '\0';
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            freqMap.put(c, freqMap.getOrDefault(c, 0)+1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : freqMap.entrySet()){
+            if(entry.getValue()>maxFreq){
+                maxFreq = entry.getValue();
+                maxChar = entry.getKey();
+            }
+        }
+
+        System.out.println("Character with maximum frequency: " + maxChar);
+        System.out.println("Maximum frequency: " + maxFreq);
+
+
+
+    }
+
+    // Leetcode 1539 Kth Missing Positive Number
+    static int linearSearch(int[] arr, int k){
+        if(arr.length == 0){
+            return -1;
+        }
+
+        // [1, 2, 3, 4], k=2
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index]<k){
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+
+
+}
